84bcf53a864d3517b6bcb6094b4d62a6bb59c6ac
Jinyan Liu <jinyan.liu@truecaller.com>
2019-03-10 16:38:46 +0100

    Add @Named

--- app/src/main/java/se/sugarest/testdaggar/MainActivity.kt
+++ app/src/main/java/se/sugarest/testdaggar/MainActivity.kt
@@ -7,24 +7,40 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import javax.inject.Named
+
+const val LOVE = "Love"
+const val HELLO = "Hello"
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var info: Info
+    @Inject
+    @field:Named(LOVE)
+    lateinit var infoLove: Info
+    @Inject
+    @field:Named(HELLO)
+    lateinit var infoHello: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerMagicBox.create().poke(this)
-        text_view.text = info.text
+        text_view.text = "${infoLove.text} ${infoHello.text}"
     }
 }
 
 @Module
 class Bag {
     @Provides
+    @Named(LOVE)
     fun sayLoveDagger2(): Info {
         return Info("Love Dagger 2")
+    }
+
+    @Provides
+    @Named(HELLO)
+    fun sayHelloDagger2(): Info {
+        return Info("Hello Dagger 2")
     }
 }
 
